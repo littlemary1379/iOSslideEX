ec9dfd345aaf70105e92187758c1630785bb5378
littlemary1379@gmail.com <littlemary1379@gmail.com>
2022-06-08 17:10:27 +0900

    Initial Commit

---
+++ SideMenuEx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenuEx
+//
+//  Created by onnoffcompany on 2022/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
